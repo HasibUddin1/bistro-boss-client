37efe339fc3fd0781e893f0b92acd9c380de296c
IconicLuck <uhasib959@gmail.com>
2023-05-24 02:38:06 +0600

    added dynamic title for routes

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Our Menu</title>
+            </Helmet>
+            <h1>You will be find here our menu</h1>
+        </div>
+    );
+};
+
+export default Menu;
