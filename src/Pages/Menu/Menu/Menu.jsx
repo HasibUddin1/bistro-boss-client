93b0bb643bf0472213dd3d946214059716d7e466
IconicLuck <uhasib959@gmail.com>
2023-05-24 03:24:09 +0600

    added parallax in menu route

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,21 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImage from '../../../assets/menu/banner3.jpg'
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 
 const Menu = () => {
     return (
-        <div>
+        <div className="mb-20">
             <Helmet>
                 <title>Bistro Boss | Our Menu</title>
             </Helmet>
-            <h1>You will be find here our menu</h1>
+            <Cover image={menuImage} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover image={menuImage} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover image={menuImage} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
