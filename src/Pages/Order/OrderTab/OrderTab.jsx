2f1d4e8b5a7da9dd34519f9d9af4b5ce320e6e03
IconicLuck <uhasib959@gmail.com>
2023-05-25 00:09:04 +0600

    added pagination in the order tab

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,15 +1,39 @@
 import FoodCard from "../../Shared/FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 
 const OrderTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
     return (
-        <div className="grid md:grid-cols-3 gap-10 mt-10 mb-10">
-            {
-                items.map(item => <FoodCard
-                    key={item._id}
-                    item={item}
-                ></FoodCard>)
-            }
+        <div>
+
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="grid md:grid-cols-3 gap-10 mt-10 mb-10">
+                        {
+                            items.map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
         </div>
     );
 };
