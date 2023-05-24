5ca512a14151970f872ba642bbc608e651e434ff
IconicLuck <uhasib959@gmail.com>
2023-05-24 23:44:01 +0600

    loaded menu data from the server and added some changes on single food card

--- src/Pages/Shared/FoodCard/FoodCard.jsx
+++ src/Pages/Shared/FoodCard/FoodCard.jsx
@@ -7,12 +7,12 @@ const FoodCard = ({item}) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
-            <div className="card-body">
+            <div className="card-body flex flex-col items-center">
                 <h2 className="card-title">{name}</h2>
                 <p className="bg-slate-900 text-white absolute top-0 right-0 mr-4 mt-4 px-4 py-2 rounded-xl">${price}</p>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-outline border-0 border-orange-400 border-b-4 text-orange-400 bg-slate-100 mt-5">Add to Cart</button>
                 </div>
             </div>
         </div>
