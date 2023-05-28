12984b4057e1ea0501f4b4091cb2124a670f071e
IconicLuck <uhasib959@gmail.com>
2023-05-28 19:49:09 +0600

    added tanstack query for loading data from the backend

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,5 +1,18 @@
-const useCart = email => {
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { useQuery } from '@tanstack/react-query'
 
+const useCart = () => {
+    const { user } = useContext(AuthContext)
+
+    const { data: cart = [], refetch } = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:5000/cart?email=${user?.email}`)
+            return response.json()
+        },
+    })
+    return [cart, refetch]
 }
 
 export default useCart;
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,19 @@ import {
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
