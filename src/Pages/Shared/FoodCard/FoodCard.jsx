a2998d5be8240b0a369ee28d811b73e1c62d6f1c
IconicLuck <uhasib959@gmail.com>
2023-05-28 15:00:24 +0600

    added a function on the add to cart button on each item

--- src/Pages/Shared/FoodCard/FoodCard.jsx
+++ src/Pages/Shared/FoodCard/FoodCard.jsx
@@ -4,6 +4,10 @@ const FoodCard = ({item}) => {
 
     const {name, image, recipe, price} = item
 
+    const handleAddToCart = item => {
+        console.log(item)
+    }
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -12,7 +16,7 @@ const FoodCard = ({item}) => {
                 <p className="bg-slate-900 text-white absolute top-0 right-0 mr-4 mt-4 px-4 py-2 rounded-xl">${price}</p>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-orange-400 border-b-4 text-orange-400 bg-slate-100 mt-5">Add to Cart</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-orange-400 border-b-4 text-orange-400 bg-slate-100 mt-5">Add to Cart</button>
                 </div>
             </div>
         </div>
--- src/Pages/Shared/NavigationBar/NavigationBar.jsx
+++ src/Pages/Shared/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
+import { FaShoppingCart } from "react-icons/fa";
 
 
 const NavigationBar = () => {
@@ -9,12 +10,12 @@ const NavigationBar = () => {
 
     const handleLogOut = () => {
         logOut()
-        .then(() => {
+            .then(() => {
 
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     const navOptions = <>
@@ -22,6 +23,12 @@ const NavigationBar = () => {
         <li><Link to='/ourMenu'>Our Menu</Link></li>
         <li><Link to='/ourShop/salad'>Our Shop</Link></li>
         <li><Link to='/secret'>Secret</Link></li>
+        <li><Link>
+            <div className="indicator">
+                <span className="indicator-item badge badge-secondary">+1</span>
+                <button className="btn bg-transparent border-none text-xl"><FaShoppingCart></FaShoppingCart></button>
+            </div>
+        </Link></li>
         {user ?
             <><button onClick={handleLogOut} className="btn btn-ghost">LogOut</button></> :
             <li><Link to='/login'>Login</Link></li>
@@ -36,14 +43,14 @@ const NavigationBar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 flex items-center">
                             {navOptions}
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul className="menu menu-horizontal px-1 flex items-center">
                         {navOptions}
                     </ul>
                 </div>
