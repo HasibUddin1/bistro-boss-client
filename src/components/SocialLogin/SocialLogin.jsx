ef5d8f11e34d6d4b37c250bcd1e5357cf39c5521
IconicLuck <uhasib959@gmail.com>
2023-05-29 21:30:11 +0600

    implemented user addition from google login by preventing duplicate user

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -18,7 +18,19 @@ const SocialLogin = () => {
             .then(result => {
                 const loggedInUser = result.user
                 console.log(loggedInUser)
-                navigate(from, { replace: true })
+                const createdUser = { name: loggedInUser.displayName, email: loggedInUser.email }
+
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                    body: JSON.stringify(createdUser)
+                })
+                    .then(res => res.json())
+                    .then(() => {
+                        navigate(from, { replace: true })
+                    })
             })
             .catch(error => {
                 console.log(error)
