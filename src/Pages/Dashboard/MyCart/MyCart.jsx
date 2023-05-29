a1af8b9d3793c2738b0abb64025b0dfde1db1988
IconicLuck <uhasib959@gmail.com>
2023-05-29 14:47:20 +0600

    displayed loaded data on tables in my cart component

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 
 const MyCart = () => {
@@ -12,10 +13,50 @@ const MyCart = () => {
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
-            <div>
+            <div className="flex gap-10 font-semibold mb-5">
                 <h3 className="text-3xl">Total Orders: {cart.length}</h3>
-                <h3 className="text-3xl">Total Price: ${parseInt(total)}</h3>
+                <h3 className="text-3xl">Total Price: ${parseInt(total).toFixed(2)}</h3>
                 <button className="btn btn-warning btn-sm">Pay</button>
+            </div>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Item Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.slice(0, 6).map((item, index )=> <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td className="text-end">${item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost bg-red-600 text-white font-bold hover:text-black"><FaTrashAlt></FaTrashAlt> </button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
             </div>
         </div>
     );
