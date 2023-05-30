085b3e96581a58499c5d5fc4682f2e513d03c857
IconicLuck <uhasib959@gmail.com>
2023-05-30 17:44:15 +0600

    added headers for sending jwt token for each email

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,11 +4,16 @@ import { useQuery } from '@tanstack/react-query'
 
 const useCart = () => {
     const { user } = useContext(AuthContext)
+    const token = localStorage.getItem('access-token')
 
     const { data: cart = [], refetch } = useQuery({
         queryKey: ['cart', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/cart?email=${user?.email}`)
+            const response = await fetch(`http://localhost:5000/cart?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
             return response.json()
         },
     })
