28698da3a8134d99e5ae04b9b9810733ebb6913f
IconicLuck <uhasib959@gmail.com>
2023-05-30 18:45:40 +0600

    implemented axios interceptor for verifying token expiration

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,20 +1,27 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
     const { user } = useContext(AuthContext)
-    const token = localStorage.getItem('access-token')
+    // const token = localStorage.getItem('access-token')
+    const [axiosSecure] = useAxiosSecure()
 
     const { data: cart = [], refetch } = useQuery({
         queryKey: ['cart', user?.email],
+        // queryFn: async () => {
+        //     const response = await fetch(`http://localhost:5000/cart?email=${user?.email}`, {
+        //         headers: {
+        //             authorization: `bearer ${token}`
+        //         }
+        //     })
+        //     return response.json()
+        // },
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/cart?email=${user?.email}`, {
-                headers: {
-                    authorization: `bearer ${token}`
-                }
-            })
-            return response.json()
+            const response = await axiosSecure(`/cart?email=${user?.email}`)
+
+            return response.data;
         },
     })
     return [cart, refetch]
